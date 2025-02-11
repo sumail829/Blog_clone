807ff48c7ef75921e61780fab8b376d1c2b53f59
suman <sumail829>
2025-02-11 08:28:39 +0545

    added Articles routes

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,17 @@ const userSchema = new mongoose.Schema({
     profilePicture: { type: String, required: true },
 });
 
+const articleSchema=new mongoose.Schema({
+    autherEmail:{type:String,required:true},
+    title:{type:String,required:true},
+    content:{type:String,required:true},
+    thumbnail:{type:String},
+
+})
+
 
 const User = mongoose.model("User", userSchema);
+const Article=mongoose.model("Article",articleSchema);
 
 
 //Users Routes(CRUD)
@@ -54,10 +63,177 @@ app.post("/users", async (req, res) => {
     }
 })
 
+// Read all users
+
+app.get("/users", async (req, res) => {
+    try {
+        const fetchUsers = await User.find();
+        return res.status(200).json({
+            message: "fetched all user success",
+            users: fetchUsers,
+        })
+
+    } catch (error) {
+        console.log("something went wrong", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+
+    }
+})
+
+//Read a single user
+
+app.get("/users/:id", async (req, res) => {
+    try {
+        const fetchUser = await User.findById(req.params.id)
+        return res.status(200).json({
+            message: "fetched single user success",
+            users: fetchUser,
+        })
+
+    } catch (error) {
+        console.log("something went wrong", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+
+    }
+})
+
+//Update the user
+app.patch("/users/:id", async (req, res) => {
+    try {
+      const userExist=await User.findById(req.params.id);
+      if(!userExist){
+        return res.status(404).json({
+            message:"User not found"
+        })
+      }
+
+      const updataedUser=await User.findByIdAndUpdate(req.params.id,req.body,{new:true})  //new true update the new value
+      return res.status(200).json({
+        message:"User success successfully"
+    
+      })
+
+
+    } catch (error) {
+        console.log("something went wrong", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+})
+
+//Delete a user
+
+app.delete("/users/:id",async(req,res)=>{
+    try {
+        const deletedUser=await User.findByIdAndDelete(req.params.id);
+        if(!deletedUser){
+            return res.status(404).json({message:"user not found"})
+        }
+
+        return res.status(200).json({
+            message:"user deleted succesfully",
+            user:deletedUser,
+        })
+
+    } catch (error) {
+        console.log("something went wrong", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+        }
+})
+
+//Article CRUD
+
+//Create Article
+
+app.post("/articles",async(req,res)=>{
+    try {
+        const createArticle=await new Article(req.body).save();
+        return res.status(200).json({
+            message:"Article cerated successfully",
+            data:createArticle,
+        })
+        
+    } catch (error) {
+        console.log("something went wrong",error)
+        return res.status(500).json({message:"Internal server error"})
+        
+    }
+})
+
+app.get("/articles",async(req,res)=>{
+    try {
+        const fetchArticles=await Article.find();
+        return res.status(200).json({
+            message:"Article fetched successfully",
+            article:fetchArticles,
+        })
+        
+    } catch (error) {
+        console.log("something went wrong",error)
+        return res.status(500).json({message:"Internal server error"})
+        
+    }
+        
+    }
+)
+// get one user
+app.get("/articles/:id",async(req,res)=>{
+    try {
+        const fetchSingleArticles=await Article.findById(req.params.id);
+        return res.status(200).json({
+            message:"Single Rticle fetched successfully",
+            article:fetchSingleArticles,
+        })
+        
+    } catch (error) {
+        console.log("something went wrong",error)
+        return res.status(500).json({message:"Internal server error"})
+        
+    }
+        
+    }
+)
+
+app.patch("/articles/:id",async(req,res)=>{
+    try {
+        const updateArticle=await Article.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!updateArticle){
+            return res.status(404).json({message:"Article not found"})
+        }
+        return res.status(200).json({message:"Article updated successfully"})
+        
+    } catch (error) {
+        console.log("something went wrong",error)
+        return res.status(500).json({message:"Internal server error"})
+        
+    }
+    }
+)
+
+app.delete("/articles/:id",async(req,res)=>{
+    try {
+        const deleteArticle=await Article.findByIdAndDelete(req.params.id);
+        if(!deleteArticle){
+            return res.status(404).json({message:"article not found"})
+        }
+        return res.status(200).json({message:"Article deleted successfully",article:deleteArticle})
+        
+    } catch (error) {
+        console.log("something went wrong",error)
+        return res.status(500).json({message:"Internal server error"})
+    }
+})
 
 
 
 //listen 
 app.listen(process.env.PORT, () => {
-    console.log("Server is running on port",process.env.PORT);
+    console.log("Server is running on port", process.env.PORT);
 })
